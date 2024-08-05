f2769b2125689f225759289fc302fab25bc8fcfc
t-nician <dev@senpai.zone>
2024-08-05 19:07:27 -0400

    more code!

--- src/account_service/app.py
+++ src/account_service/app.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
@@ -14,6 +15,50 @@ async def account_type_to_api_model(
         case PlatformAccountType.MORDHAU_ACCOUNT:
             return MordhauAccountObject
     return None
+
+
+async def api_create_primary_account(
+    primary_account: PrimaryAccountObject
+) -> PrimaryAccountObject:
+    primary_account_model = await create_primary_account(
+        account_id=primary_account.authority_account_id,
+        account_type=primary_account.authority_account_type
+    )
+    
+    return PrimaryAccountObject(
+        authority_account_id=primary_account_model.authority_account_id,
+        authority_account_type=primary_account_model.authority_account_type
+    )
+    
+
+async def api_create_platform_account(
+    platform_account: MordhauAccountObject | DiscordAccountObject
+) -> MordhauAccountObject | DiscordAccountObject:
+    platform_account_model = await create_platform_account(
+        primary_account=await get_primary_account(
+            account_id=platform_account.primary_account.authority_account_id,
+            account_type=platform_account.primary_account.authority_account_type
+        ),
+        
+        account_id=platform_account.account_id,
+        account_type=platform_account.account_type,
+        account_name=platform_account.account_name
+    )
+    
+    platform_account_class = await account_type_to_api_model(
+        account_type=platform_account.account_type
+    )
+    
+    return platform_account_class(
+        primary_account=PrimaryAccountObject(
+            authority_account_id=platform_account_model.primary_account.authority_account_id,
+            authority_account_type=platform_account_model.primary_account.authority_account_type
+        ),
+        
+        account_id=platform_account.account_id,
+        account_type=platform_account.account_type,
+        account_name=platform_account.account_name
+    )
 
 
 async def api_get_primary_account(
@@ -60,7 +105,22 @@ async def api_get_platform_account(
         account_type=account_type
     )
 
+
 def hook_api_endpoints(app: FastAPI):
+    app.add_api_route(
+        "/account-service/create-primary-account",
+        response_model=PrimaryAccountObject,
+        endpoint=api_create_primary_account,
+        methods=["POST"]
+    )
+    
+    app.add_api_route(
+        "/account-service/create-platform-account",
+        response_model=DiscordAccountObject | MordhauAccountObject,
+        endpoint=api_create_platform_account,
+        methods=["POST"]
+    )
+    
     app.add_api_route(
         "/account-service/get-primary-account-by",
         response_model=PrimaryAccountObject,
